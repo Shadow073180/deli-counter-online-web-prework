d6c70320ecce84bf95ada382fc0aa934679587cc
David masterson <david73180@gmail.com>
2018-09-20 03:10:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -36,10 +36,14 @@ end
 
 
 def now_serving(array)
+  if array.length > 0
   puts "Currently serving #{array.first}."
-  array.shift
+  array.
+  
+  else
+    puts "There is nobody waitng to be served!"
 end
-
+end
     
 
 
