8828cd714d98d6f614e3e924291a579a0e62da89
David masterson <david73180@gmail.com>
2018-09-20 02:37:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,9 +23,9 @@ end
 def take_a_number(array, person)
   katz_deli.push(person)
   i = array.length -1
-  puts "Welcome #{katz_deli[i]}. you are number #{i} in line. "
+  puts "Welcome #{katz_deli[katz_deli[i]]}. you are number #{i} in line. "
 end
-end
+
     
 
 
